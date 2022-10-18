39bbf9c76bf8541fa33f695abb7e2b3051b80a5c
Jeff Gonzalez <jeff@hypertheory.com>
2022-10-18 11:05:13 -0400

    Before Tuesday Break

--- DevelopersSolution/DevelopersApi/Controllers/DevelopersController.cs
+++ DevelopersSolution/DevelopersApi/Controllers/DevelopersController.cs
@@ -2,6 +2,9 @@
 using Microsoft.AspNetCore.Mvc;
 using DevelopersApi.Adapters;
 using DevelopersApi.Domain;
+using MongoDB.Driver;
+using MongoDB.Driver.Linq;
+using MongoDB.Bson;
 
 namespace DevelopersApi.Controllers;
 
@@ -14,29 +17,52 @@ public class DevelopersController : ControllerBase
         _mongoAdapter = mongoAdapter;
     }
 
+    // PUT /on-call-developer
+    [HttpPut("/on-call-developer")]
+    public async Task<ActionResult> AssignOnCallDeveloper([FromBody] DeveloperSummaryModel request)
+    {
+        var objectId = ObjectId.Parse(request.Id);
+        var whereIsOnCallFilter = Builders<DeveloperEntity>.Filter.Where(d => d.IsOnCallDeveloper == true);
+        var updateToUnsetOnCallDeveloper = Builders<DeveloperEntity>.Update.Set(d => d.IsOnCallDeveloper, false);
+
+        await _mongoAdapter.Developers.UpdateOneAsync(whereIsOnCallFilter, updateToUnsetOnCallDeveloper);
+        // If there is one that is already on call, take them "off call"
+
+        var newIsOnCallFilter = Builders<DeveloperEntity>.Filter.Where(d => d.Id == objectId);
+        var newInOnCallUpdate = Builders<DeveloperEntity>.Update.Set(d => d.IsOnCallDeveloper, true);
+
+        await _mongoAdapter.Developers.UpdateOneAsync(newIsOnCallFilter, newInOnCallUpdate);
+        // we need to change the developer passed in here's oncall property to true and save it.
+        return Accepted();
+    }
 
 
     // GET /on-call-developer
     [HttpGet("/on-call-developer")]
-    public ActionResult GetOnCallDeveloper()
+    public async Task<ActionResult> GetOnCallDeveloper()
     {
-        
-        var response = new DeveloperDetailsModel("1", "Jeff", "Gonzalez", "555-1212", "jeff@hypertheory.com");
-        
-       
+
+        var response = await _mongoAdapter.Developers.AsQueryable()
+            .Where(d => d.IsOnCallDeveloper == true)
+            .Select(d => new DeveloperDetailsModel(d.Id.ToString(), d.FirstName, d.LastName, d.Phone, d.Email))
+            .SingleOrDefaultAsync(); // 0 or 1. If it > 1, BLAMMO!
 
         return Ok(response); // 200 Status code.
     }
 
     [HttpGet("/developers")]
-    public ActionResult GetAllDevelopers()
+    public async Task<ActionResult> GetAllDevelopers()
     {
         var response = new CollectionResponse<DeveloperSummaryModel>();
-        response.Data = new List<DeveloperSummaryModel>()
-        {
-            new DeveloperSummaryModel("1", "Jeff", "Gonzalez", "jeff@hypertheory.com"),
-            new DeveloperSummaryModel("2", "Sue", "Jones", "sue@aol.com")
-        };
+        
+
+        var data = _mongoAdapter.Developers.AsQueryable()
+            .Select(d => new DeveloperSummaryModel(
+                d.Id.ToString(), d.FirstName, d.LastName, d.Email));
+
+        response.Data = await data.ToListAsync();
+
+        
         return Ok(response);
     }
 
@@ -49,12 +75,12 @@ public class DevelopersController : ControllerBase
             LastName = request.LastName,
             Email = request.Email,
             Phone = request.Phone,
-            IsOnCallDeveloper = true
+            IsOnCallDeveloper = false
         };
 
         await _mongoAdapter.Developers.InsertOneAsync(developerToAdd);
 
-        
-        return StatusCode(201); // "Good. Ok. I created this.
+        var response = new DeveloperSummaryModel(developerToAdd.Id.ToString(), developerToAdd.FirstName, developerToAdd.LastName, developerToAdd.Email);
+        return StatusCode(201, response); // "Good. Ok. I created this.
     }
 }
